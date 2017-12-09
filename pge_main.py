1d6e4424b7ce8b8e41242136eb7610dda9b5d644
Derek Scharton <dscharton@financialengines.com>
2017-12-08 21:57:33 -0800

    Move all CSV files to a separate folder and more.
    Refactor some function names. Prep folder naming to variable for a
    future move to ConfigParser.

--- pge_main.py
+++ pge_main.py
@@ -1,31 +1,19 @@
-#!/usr/local/bin/python3
 import os
 import csv
 import sqlite3
 import zipfile
 
 
-# Steps to move file and prepare
-# Copy "DailyUsagexxx" from Downloads to pge folder
-# Unzip the file
-# Copy from folder DailyUsage to pge (parent)
-# Remove first 5 lines from each file (maybe do that before the copy)
-# Delete the zip and temp folder
-
-folderlocation = '.'
+csvdatafolder = './csvdata'
 
 elecfile = 'pge_electric_interval_data'
 gasfile = 'pge_gas_interval_data'
 sample = 'pge_electric_interval_data_4575820864_2017-06-10_to_2017-07-09.csv'
 eleccsv = 'electric.csv'
 
-def build_electric_csv():
+def load_electric():
 
     header = 'TYPE,START DATE,END DATE,USAGE,UNITS,COST,NOTES'
-
-    with open(eleccsv, 'w') as eleccsvinit:
-        print('initialized new {}'.format(eleccsv))
-
 
     conn = sqlite3.connect('energy.sqlite')
     csr = conn.cursor()
@@ -33,23 +21,19 @@ def build_electric_csv():
     conn.commit()
     data = []
 
-    for dir, subdir, files in os.walk('./'):
+    for dir, subdir, files in os.walk(csvdatafolder):
         for f in files:
             if f.startswith(elecfile):
                 fullfile = dir + '/' + f
                 with open(fullfile, 'r+') as pgefile:
                     reader = csv.reader(pgefile)
                     for row in reader:
-                        # skip header/text
-                        # if str.isalnum(row[0]):
-                        #     # print('skipping {}'.format(row))
-                        #     continue
+                        # Each real record starts with "Electric" as the first item
                         if len(row) != 8 or str(row[0]).startswith('Electric') is False:
                             continue
 
                         elecdate = str('{}').format(row[1])
 
-# datenum = str("{}{}{}").format(v[0:4], v[5:7], v[8:10])
                         elecdatenum = elecdate[0:4] + elecdate[5:7] + elecdate[8:10]
                         elechr = str('{} {}:00').format(elecdate, row[2])
                         usage = row[4]
@@ -66,26 +50,20 @@ def build_electric_csv():
     conn.commit()
     conn.close()
 
-def build_gas_csv():
+def load_gas():
 
     conn = sqlite3.connect('energy.sqlite')
     csr = conn.cursor()
     csr.execute('delete from gas')
     data = []
 
-    for dir, subdir, files in os.walk('./'):
+    for dir, subdir, files in os.walk(csvdatafolder):
         for f in files:
             if f.startswith(gasfile):
                 fullfile = dir + '/' + f
-                # print('writing {} to new csv'.format(fullfile))
                 with open(fullfile, 'r+') as pgefile:
                     reader = csv.reader(pgefile)
                     for row in reader:
-                        # skip header/text
-                        # if str.isalnum(row[0]):
-                        #     # print('skipping {}'.format(row))
-                        #     continue
-
                         if len(row) != 6 or str(row[0]).startswith('Natural') is False:
                             continue
 
@@ -106,23 +84,31 @@ def build_gas_csv():
 
 
 def process_zip_file(folder):
+    """Look for any DailyUsageData files in the folder tree
+
+    Files will look like pge_electric_interval...<date range>.csv
+    or pge_gas_interval...<date range>.csv.
+
+
+    """
     for dir, subdir, files in os.walk(folder):
         for f in files:
             if f.startswith('DailyUsageData'):
                 zip_file = folder + f
 
-                with zipfile.ZipFile(zip_file) as zip_pge:
+                with zipfile.ZipFile(file=zip_file, mode='r') as zip_pge:
                     pge_files = zip_pge.namelist()
                     for pge_file in pge_files:
                         # extraction will land in the pge folder because
                         # this is where the python script originates
-                        zip_pge.extract(pge_file)
+                        zip_pge.extract(member=pge_file, path=csvdatafolder)
 
 
 if __name__ == '__main__':
-
-    download_folder = '/Users/dscharton/Downloads/'
-    process_zip_file(download_folder)
-    build_electric_csv()
-    build_gas_csv()
-    # write_to_db()
+    if False:
+        print('hi')
+    else:
+        download_folder = '/Users/dscharton/Downloads/'
+        process_zip_file(download_folder)
+        load_electric()
+        load_gas()
